647d4869faf7f9c2943180316415b1df6eb76943
Eric Raymund Rex Sylvester <58367989+ericraymundrex@users.noreply.github.com>
2022-04-28 15:33:23 +0530

    re-arranged, added the add new

--- front-end-ecom/src/App.js
+++ front-end-ecom/src/App.js
@@ -2,14 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+import Add from './components/Add'
 import { Fragment } from 'react';
 
 
 function App() {
+  const localData = localStorage.getItem("token");
   return (
     <Fragment>
-      <Login/>
-      <SignUp/>
+      {localData ? <Add /> : <div><Login/> <SignUp/></div>}
+      
     </Fragment>
   );
 }
---
+++ front-end-ecom/src/components/Add.js
@@ -0,0 +1,132 @@
+import { Fragment, useState } from "react"
+import axios from "axios"
+
+const AddItem = () =>{
+    const categories = [{'id':1,'value':'Electronics'},
+                        {'id':2,'value':'Men Fashion'},
+                        {'id':3,'value':'Women Fashion'},
+                        {'id':4,'value':'Mobile Phone'},
+                        {'id':5,'value':'Sports Item'},
+                        {'id':6,'value':'Men Footware'},
+                        {'id':7,'value':'Women Footware'}]
+    
+    const [name,setName]=useState('')
+    const [quantity,setQty]=useState('')
+    const [category,setCat]=useState('')
+    const [description,setDes]=useState('')
+    const [price,setPrice]=useState('')
+                        
+    const nameChangeHandler = (event) =>{
+      setName(event.target.value)
+    }
+    const qtyChangeHandler = (event) =>{
+      setQty(event.target.value)
+    }
+    const priceChangeHandler = (event) =>{
+      setPrice(event.target.value)
+
+    }
+    const catChangeHandler = (event) =>{
+      setCat(event.target.value)
+
+    }
+    const desChangeHandler = (event) =>{
+      setDes(event.target.value)
+
+    }
+    
+    const submitHandler = async(event)=>{
+      event.preventDefault();
+        let message=await axios.post("http://localhost:5000/merchant",{
+          id:'',
+          name:name,
+          qty:quantity,
+          cat:category,
+          des:description,
+          price:price,
+          
+      });
+      console.log(message)
+    }
+    return(
+        <Fragment>
+            <form onSubmit={submitHandler} >
+        <h3>Add New Item Page</h3>
+            <div className="container">
+
+        <div className="mb-3 mt-3">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            placeholder="Enter Your Name"
+            onChange={nameChangeHandler}
+          />
+        </div>
+
+        <div className="mb-3 mt-3">
+          <label htmlFor="qty">Quantity</label>
+          <input
+            type="text"
+            name="qty"
+            id="qty"
+            className="form-control"
+            placeholder="Enter Quantity"
+            onChange={qtyChangeHandler}
+          />
+        </div>
+        <div className="mb-3 mt-3">
+          <label htmlFor="price">Price</label>
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="form-control"
+            placeholder="Enter Price"
+            onChange={priceChangeHandler}
+          />
+        </div>
+
+        <div className="mb-3 mt-3">
+          <label htmlFor="cat">Category : </label>
+          <select name='cat' 
+            onChange={catChangeHandler}
+          >
+          {categories.map((item)=>(
+              <option key={item.id} value={item.value}>{item.value}</option>
+          ))}
+            
+          </select>
+
+        </div>
+                  {/* <p>{{categories}}</p> */}
+
+        <div className="mb-3 mt-3">
+          <label htmlFor="des">Description</label>
+          <input
+            type="text"
+            name="des"
+            id="des"
+            className="form-control"
+            placeholder="Enter Description"
+            onChange={desChangeHandler}
+          />
+        </div>
+        
+        <div className="btn-container">
+        
+        <button type="submit" className="btn btn-primary">
+          Add Item
+        </button>
+        </div>
+        
+        </div>
+      </form> 
+      
+        </Fragment>
+    )
+}
+
+export default AddItem;
