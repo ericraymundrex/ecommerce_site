04d2c2f4f55c591e37d4a76e8155b7e8cbdb969a
Eric Raymund Rex Sylvester <58367989+ericraymundrex@users.noreply.github.com>
2022-04-28 11:02:11 +0530

    added the authentication for the user page

--- Model.py
+++ Model.py
@@ -1,6 +1,7 @@
-
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Ecommerce.sqlite3'
@@ -16,25 +17,68 @@ class Products(db.Model):
     product_description=db.Column(db.String(1000))
     product_price=db.Column(db.Integer)
 
-    def __init__(self,product_name,product_category,product_available_qty,product_price,product_description):
+    # purchase=relationship('Purchase',backref="products")
+
+    merchants=db.Column(db.Integer,ForeignKey("merchant.id"))
+
+    def __init__(self,product_name,product_category,product_available_qty,product_price,product_description,merchants):
         self.product_name = product_name
         self.product_category = product_category
         self.product_price = product_price
         self.product_available_qty = product_available_qty
         self.product_description = product_description
+        self.merchants=merchants
 
 
 
 class Merchant(db.Model):
     merchant_id=db.Column('id',db.Integer,primary_key=True)
+    merchant_name=db.Column(db.String(100))
     merchant_email=db.Column(db.String(100))
     hash=db.Column(db.String(100))
     salt=db.Column(db.String(100))
+    isdeleted=db.Column(db.String(1))
+    products=relationship('Products',backref="merchant")
 
-    def __init__(self,merchant_email,hash,salt):
-        self.merchant_email=merchant_email
+    def __init__(self,merchant_email,hash,salt,merchant_name):
+        self.merchant_name=merchant_name.lower()
+        self.merchant_email=merchant_email.lower()
         self.hash=hash
         self.salt=salt
+        self.isdeleted=False
+
+class Users(db.Model):
+    user_id=db.Column('id',db.Integer,primary_key=True)
+    user_email=db.Column(db.String(100))
+    hash=db.Column(db.String(100))
+    salt=db.Column(db.String(100))
+    isdeleted=db.Column(db.String(1))
+
+    purchase_id=relationship('Purchase',backref="users") 
+    
+    def __init__(self,user_email,hash,salt):
+        self.user_email=user_email
+        self.hash=hash
+        self.salt=salt
+        self.isdeleted=False
+
+class Purchase(db.Model):
+    purchase_id=db.Column('id',db.Integer,primary_key=True)
+    
+    product_id=db.Column(db.Integer,ForeignKey('products.id'))
+    user_id=db.Column(db.Integer,ForeignKey('users.id'))
+    
+    inCart=db.Column(db.LargeBinary)
+    date=db.Column(db.Date)
+    status=db.Column(db.String(10))
+    quantity=db.Column(db.Integer)
+    def __init__(self, product_id,user_id,inCart,date,status,quantity):
+        self.product_id=product_id
+        self.user_id=user_id
+        self.inCart=inCart
+        self.date=date
+        self.status=status
+        self.quantity=quantity
 
 db.create_all()
 
--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
-from turtle import update
-from Model import Products, Merchant as Merchant_model,db,app
+from Model import Products, Merchant as Merchant_model,Users,Purchase,db,app
 from flask import request, jsonify
 import bcrypt
 import jwt
@@ -8,12 +6,10 @@ import datetime
 from functools import wraps
 
 
-def token_required(f):
+def token_required_merchant(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        # args=token.parse_args()
-        token = request.args.get("token")
-        # print(token)
+        token = request.headers.get("token")
         try:
             jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
         except:
@@ -24,9 +20,9 @@ def token_required(f):
 
 class Merchant:
 
-    def addItem(id,name,qty,cat,price,des):
-        print("Product listing "+str(Products.product_id))
-
+    def addItem(id,name,qty,cat,price,des,merchant):
+        merchant_session=db.session.query(Merchant_model.merchant_id).filter_by(merchant_name=merchant.lower())
+        print(merchant_session)
         if id != '':
             updateItem = Products.query.get(id)
             updateItem.product_price=price            
@@ -34,13 +30,14 @@ class Merchant:
             updateItem.product_available_qty=qty
             updateItem.product_category=cat
             updateItem.product_description=des
+            updateItem.merchants=merchant_session
             db.session.commit()
 
             return "SuccessFully Updated"
         # .update(product_name=name,product_category=cat,product_available_qty=qty,product_price=price ,product_description=des)
 
         else:
-            newitem = Products(product_name=name,product_category=cat,product_available_qty=qty,product_price=price,product_description=des)
+            newitem = Products(product_name=name,product_category=cat,product_available_qty=qty,product_price=price,product_description=des,merchants=merchant_session)
             db.session.add(newitem)
             db.session.commit()
             return "successfully added"
@@ -49,24 +46,22 @@ class Merchant:
     def deleteItem(id):
         Products.query.filter_by(product_id=id).delete()
         db.session.commit()
-        return "Deleted"
+        return {"message":"Deleted"}
 
     def ListItem():
         pass
 
-    def signup(email,password):
-        exist=db.session.query(Merchant_model.merchant_id).filter_by(merchant_email=email).first() is not None
-        print(exist)
+    def signup(email,password,name):
+        exist=db.session.query(Merchant_model.merchant_id).filter_by(merchant_email=email.lower()).first() is not None
         if exist:
             return {"message":"There is a user exist"}
         else:
-            print(email,password)
+            # print(email,password)
             salt = bcrypt.gensalt()
             hash = bcrypt.hashpw(password.encode('utf-8'), salt)
-            hash = str(hash).split("'")[1]
-            salt = str(salt).split("'")[1]
+
             try:
-                new_merchant=Merchant_model(merchant_email=email,hash=hash,salt=salt)
+                new_merchant=Merchant_model(merchant_email=email,hash=hash,salt=salt,merchant_name=name)
                 db.session.add(new_merchant)
                 db.session.commit()
             except:
@@ -74,46 +69,147 @@ class Merchant:
             return {"message":"Merchant successfully created"}
     
     def login(email,password):
-        data=Merchant_model.query.filter_by(merchant_email=email).first()
+        try:
+            data=Merchant_model.query.filter_by(merchant_email=email.lower()).first()
+        except:
+            return {"message":"There is a problem in Database connection"}
         email=data.merchant_email
+        name=data.merchant_name
         hash=data.hash
         salt=data.salt
-        hashed = bcrypt.hashpw(password.encode('utf-8'), salt.encode('utf-8'))
-        hashed = str(hashed).split("'")[1]
-        if (hashed.encode('utf-8') == hash.encode('utf-8')):
-            token = jwt.encode({"user": email, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
-            return {"token":token}
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        if (hashed == hash):
+            token = jwt.encode({"email": email,"name":name, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+            return jsonify({"token":token})
         else:
-            {"message":"Login is not a successful attempt"}
+            jsonify({"message":"Login is not a successful attempt"})
 
-@app.route("/signup",methods=["POST"])
+
+
+@app.route("/merchant/signup",methods=["POST"])
 def signup():
     email=request.form['email']
     password=request.form['password']
-    return Merchant.signup(email,password)
+    name=request.form['name']
+    return Merchant.signup(email,password,name)
 
-@app.route("/login",methods=["POST"])
+@app.route("/merchant/login",methods=["POST"])
 def login():
     email=request.form['email']
     password=request.form['password']
     return Merchant.login(email,password)
 
-@token_required
-@app.route('/',methods=['POST'])
+@app.route('/merchant',methods=['POST'])
+@token_required_merchant
 def root():
+    token = request.headers.get("token")
     id = request.form['id']
     name = request.form['name']
-    qty = request.form['qty']
+    qty = int(request.form['qty'])
     cat = request.form['cat']
     des = request.form['des']
-    price = request.form['price']
-    return Merchant.addItem(id,name,qty,cat,price,des)
+    price = int(request.form['price'])
+    merchant=jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+    print(merchant)
+    return Merchant.addItem(id,name,qty,cat,price,des,merchant['name'])
 
-
-@token_required
-@app.route('/<int:id>')
+@app.route('/merchant/<int:id>',methods=["DELETE"])
+@token_required_merchant
 def delete(id):
     return Merchant.deleteItem(id)
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+class User:
 
+    def signup(email,password):
+        exist=db.session.query(Users.user_id).filter_by(user_email=email).first() is not None
+        print(exist)
+        if exist:
+            return {"message":"There is a user exist"}
+        else:
+            print(email,password)
+            salt = bcrypt.gensalt()
+            hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+            try:
+                new_user=Users(user_email=email,hash=hash,salt=salt)
+                db.session.add(new_user)
+                db.session.commit()
+            except:
+                return {"message":"Error in Database"}
+            return {"message":"User successfully created"}
+    
+    def login(email,password):
+        try:
+            data=Users.query.filter_by(user_email=email).first()
+            print(data.user_email)
+        except:
+            return {"message":"There is a problem in Database connection"}
+        email=data.user_email
+        hash=data.hash
+        salt=data.salt
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        if (hashed == hash):
+            token = jwt.encode({"email": email, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+            return jsonify({"token":token})
+        else:
+            jsonify({"message":"Login is not a successful attempt"})
+    
+    def purchase(product_id,entered_number_of_products,user_id,inCart,date,status):
+
+        product_available=db.session.query(Products.available_qty).filter_by(product_id=product_id).first() is not None
+        if product_available is None:
+            return {"message":"product is is wrong"}
+        elif product_available <entered_number_of_products:
+            return {"message":"Available product is "+str(product_available)}
+        else:
+            new_purchase=Purchase(product_id=product_id,user_id=user_id,inCart=inCart,date=date,status=status,quantity=entered_number_of_products)
+            db.session.add(new_purchase)
+            db.session.commit()
+            return {"message":"product entered successfully"}
+@app.route("/user/signup",methods=["POST"])
+def signup_user():
+    email=request.form['email']
+    password=request.form['password'] 
+    return User.signup(email,password)
+
+@app.route("/user/login",methods=["POST"])
+def login_user():
+    email=request.form['email']
+    password=request.form['password'] 
+    return User.login(email,password) 
+
+@app.route("/user/purchase",methods=["POST"])
+def purchase():
+    product_id=request.form['project_id']
+    quantity=request.form['quantity']
+    user_id=request.form['user_id']
+    inCart=request.form['inCart']
+    date=request.form['date']
+    status=request.form['status']
+    return User.purchase(product_id,quantity,user_id,inCart,date,status)
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+
+class Page():
+    def home():
+        return {"message":"home page"}
+    
+    def sections(shopby,pricerange_higher_limit,pricerange_lower_limit,brandname):
+        # filter by sections -> routes
+        # params -> pricerange, brandname
+        arr=[]
+        results=db.session.query(Products).filter(Products.product_price<=pricerange_higher_limit & Products.product_price >pricerange_higher_limit & Products.product_category== shopby)
+        for result in results:
+            arr.append(result)
+        return {"data":arr}
+        
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
